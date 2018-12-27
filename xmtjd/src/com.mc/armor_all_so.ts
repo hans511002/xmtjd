b26951f38898b5fefdc1cf7a67eaa783743e2172
hans511002 <hans511002@sohu.com>
2018-12-27 23:08:14 +0800

    t

--- xmtjd/src/com.mc/armor_all_so.ts
+++ xmtjd/src/com.mc/armor_all_so.ts
@@ -1,7 +1,6 @@
 class armor_all_so extends egret.Sound {
 
-
     public constructor() {
         super();
     }
-} 
+}
--- xmtjd/src/com.mc/card_mc.ts
+++ xmtjd/src/com.mc/card_mc.ts
@@ -1,31 +1,24 @@
-﻿package 
-{
-    import flash.display.*;
-    import flash.text.*;
+﻿class card_mc extends std.MovieClip {
+    bg_cl: std.MovieClip;
+    cat_cl: std.MovieClip;
+    des_tx: std.MCText;
+    icon_cl: std.MovieClip;
+    n_tx: std.MCText;
+    over_cl: std.MovieClip;
+    telo: std.MovieClip;
+    title_tx: std.MCText;
+    wear_cl: std.MovieClip;
 
-    dynamic public class card_mc extends MovieClip
-    {
-        public var bg_cl:MovieClip;
-        public var cat_cl:MovieClip;
-        public var des_tx:TextField;
-        public var icon_cl:MovieClip;
-        public var n_tx:TextField;
-        public var over_cl:MovieClip;
-        public var telo:MovieClip;
-        public var title_tx:TextField;
-        public var wear_cl:MovieClip;
+    public constructor() {
+        super();
+        // addFrameScript(0, this.frame1);
+        return;
+    }// end function
 
-        public function card_mc()
-        {
-            addFrameScript(0, this.frame1);
-            return;
-        }// end function
+    // function frame1()
+    // {
+    //     stop();
+    //     return;
+    // }// end function
 
-        function frame1()
-        {
-            stop();
-            return;
-        }// end function
-
-    }
 }
